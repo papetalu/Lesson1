2ad045e1e5e267cc0a8abacb6b8fef07a3ce150a
Liuba <pl@mail.ru>
2022-09-17 10:53:46 +0300

    Zadacha 1

---
+++ Zadacha01/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите значение 1: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение 2: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x * x == y)
+Console.WriteLine("yes");
+else
+Console.WriteLine("no");
