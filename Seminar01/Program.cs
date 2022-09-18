7d8bc4ad294c95d53725793e160e57578ab15113
Liuba <pl@mail.ru>
2022-09-18 09:39:51 +0300

    Sem 2

--- Seminar01/Program.cs
+++ Seminar01/Program.cs
@@ -2,3 +2,17 @@
 Console.Write("Введите значение: ");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Результат: " + n * n);
+
+// Console.Clear();
+// Console.Write("Введите год: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// // Проверить год високосный или нет?
+// // (кратен 4 и не кратен 100 или кратен 400)
+
+// // && (and) - и (*)
+// // || (or) - или (+)
+
+// if (n % 4 == 0 && n % 100 != 0 || n % 400 == 0)
+//     Console.WriteLine("yes");
+// else
+//     Console.WriteLine("no");
--- Zadacha03/Program.cs
+++ Zadacha03/Program.cs
@@ -1,5 +1,11 @@
 ﻿Console.Clear();
-Console.Write("Введите значение: ");
-int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a * (-1);
+while (b <= a)
+{
+    Console.Write(b);
+    b++;
+}
 
 
